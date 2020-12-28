b75f0a38581dd37d82f6b0deb896cd163a8b3717
Firas Chbiki <firas.chbiki@sofrecom.com>
2020-12-28 22:05:50 +0100

    improving pre setup script

--- pre-setup.sh
+++ pre-setup.sh
@@ -32,6 +32,7 @@ read workers
 
 # Generating ssh key / ignoring if already exist
 ssh-keygen -t rsa -f $HOME/.ssh/id_rsa  -q -P ""  <<< y
+echo # blanc line
 
 # Assuming that all nodes have the same password, asking the user to enter it:
 read -s -p "Please insert your nodes' Password: " password
@@ -47,24 +48,25 @@ do
     echo
     read -s -p "Password (again): " password2
 done
-
+echo # blanc line
 # Copy the ssh key to the master
 sshpass -p "$password" ssh-copy-id -o StrictHostKeychecking=no $username@$pvip 
- echo -n "Copied ssh-key to master node Node.\n"
+ echo -e "Copied ssh-key to master node."
 
 
-echo -n "How many worker nodes do you have?: "
-read workers
+
 for (( i = 1; i < workers+1; i++ ))
 do
 
    echo -n "enter ther worker number $i's ip: "
    read ip
    echo "k8s-worker-$i ansible_host=$ip  ansible_user=$username" >> hosts
-   echo -n "Node worker-$i added to hosts file.\n"
+   echo -n "Node worker-$i added to hosts file."
+   echo # blanc line
 
    sshpass -p "$password" ssh-copy-id -o StrictHostKeychecking=no $username@$ip 
-   echo -n "Copied ssh-key to worker-$i Node.\n"
+   echo -n "Copied ssh-key to worker-$i Node."
+   echo # blanc line
 
 done
 
@@ -88,6 +90,8 @@ sleep 1
 echo -ne '########################   (100%) Setup completed! \r'
 echo -ne '\n'
 
-echo -e "Please Note that this Script back your current setup up if launched again, and it will overwrite any backed up configuration\n"
-echo -e "##############################################################################################\n"
+echo -e "Please Note that this Script back your current setup up if launched again, and it will overwrite any backed up configuration"
+echo # blanc line
+echo -e "##############################################################################################"
+echo # blanc line
 echo -e "you can check the  env_variables and hosts file to verify your actual configuration :)"
