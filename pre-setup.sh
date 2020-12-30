e31591c437e7250e6f07813ee7fd61f529d4dcf6
Firas Chbiki <firas.chbiki@sofrecom.com>
2020-12-30 19:28:33 +0100

    fixed pre setup bugs

--- pre-setup.sh
+++ pre-setup.sh
@@ -109,7 +109,7 @@ else
   while true; do
       read -p "Do you wish to taint your master Node?:(yes/no)" yn
       case $yn in
-          [Yy]* ) sed -i "s/TAINTED: NO/TAINTED: yes/g" env_variables; break;;
+          [Yy]* ) sed -i "s/TAINTED: no/TAINTED: yes/g" env_variables; break;;
           [Nn]* ) break;;
           * ) echo "Please answer with yes or no.";;
       esac
@@ -169,7 +169,7 @@ done
 while true; do
     read -p "Are you using docker behind a proxy?:(yes/no)" yn
     case $yn in
-        [Yy]* ) sed -i "s/DOCKER_PROXY_ENABLE: NO/DOCKER_PROXY_ENABLE: yes/g" env_variables;
+        [Yy]* ) sed -i "s/docker_proxy: no/docker_proxy: yes/g" env_variables;
                 cp templates/docker_proxy-template  docker-proxy.conf
                 # Reading HTTP_PROXY
                 echo -n "Enter your docker http_proxy: "
